2cea77916308852e5828ab3503e773e1a82fb517
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-24 23:55:06 +0530

    login page update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-
+// import { useJavaPoints } from '../Java/JavaPointsContext';
 const Login = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
