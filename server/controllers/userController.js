d83218b283b43ea34305334483f735e22f048e67
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-04-26 17:27:09 +0530

    made changes for the backend deployment

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,11 +21,14 @@ export const updateUserPoints = async (req, res) => {
 
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User not found" });
-
-    user.points = points;
-    await user.save();
-
-    res.status(200).json({ message: "Points updated successfully", points });
+    if(user.points >= 170){
+      return res.status(400).json({ message: "User already has 170 points" });
+    } else {
+      user.points = points;
+      await user.save();
+      res.status(200).json({ message: "Points updated successfully", points });
+    }
+   
   } catch (error) {
     console.error("Error updating points:", error);
     res.status(500).json({ message: "Server error" });
